afbc1402208ef9789618c77b32f41291527e9395
ngocphuong1809 <79855252+ngocphuong1809@users.noreply.github.com>
2022-04-08 12:54:48 +0700

    Update rsimfi.py

--- jesse/rsimfi.py
+++ jesse/rsimfi.py
@@ -2,12 +2,12 @@ from typing import Union
 
 import numpy as np
 import talib
+import jesse.indicators as ta
+
 
 from jesse.helpers import slice_candles
 
-# f_rsimfi(_period, _multiplier, _tf) => security(syminfo.tickerid, _tf, sma(((close - open) / (high - low)) * _multiplier, _period) - rsiMFIPosY)
-
-def rsimfi(candles: np.ndarray, period: int = 60, multiplier: float = 250, sequential: bool = False) -> Union[float, np.ndarray]:
+def rsimfi(candles: np.ndarray, period: int = 60, multiplier: float = 250.0, sequential: bool = False) -> Union[float, np.ndarray]:
     """
     MFI - Money Flow Index
 
@@ -18,6 +18,8 @@ def rsimfi(candles: np.ndarray, period: int = 60, multiplier: float = 250, seque
     :return: float | np.ndarray
     """
     candles = slice_candles(candles, sequential)
-    rf = talib.SMA((((candles[:, 2] - candles[:, 1])/(candles[:, 3] - candles[:, 4]))* multiplier), period)
+    rf = ta.sma(((candles[:, 2] - candles[:, 1])/(candles[:, 3] - candles[:, 4]))* multiplier, period)
 
-    return rf if sequential else rf[-1]
+    return rf
+
+  
